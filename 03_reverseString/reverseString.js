5399b6782d03d1d50cbfacfd654f9b817e36b4f4
Fatima Hassan <110312999+FatimaHassan26@users.noreply.github.com>
2024-06-21 22:22:44 -0500

    Complete exercise three

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(input) {
+    let reversedString = "";
+    for(let i = input.length; i>=0; i--){
+        reversedString+= input.charAt(i);
+    }
+    return reversedString
 };
 
 // Do not edit below this line
